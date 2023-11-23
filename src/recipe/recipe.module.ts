1da5d0b1fb9f878fbadd7511ca7a7c5cc843dc79
Lucas Campolim <lucascampolim@protonmail.com>
2023-11-23 19:16:48 -0300

    feat: operacoes de crud da rota recipes

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { RecipeController } from './recipe.controller';
 import { RecipeService } from './recipe.service';
+import { RecipeSchema } from './schemas/recipe.schema';
 
 @Module({
+	imports: [MongooseModule.forFeature([{ name: 'Recipe', schema: RecipeSchema }])],
 	controllers: [RecipeController],
 	providers: [RecipeService]
 })
--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Recipe } from './schemas/recipe.schema';
 
 @Injectable()
-export class RecipeService {}
+export class RecipeService {
+	constructor(
+		@InjectModel(Recipe.name)
+		private recipeModel: mongoose.Model<Recipe>
+	) {}
+
+	async findAll(): Promise<Recipe[]> {
+		const recipes = await this.recipeModel.find();
+		return recipes;
+	}
+
+	async create(recipe: Recipe): Promise<Recipe> {
+		const res = await this.recipeModel.create(recipe);
+		return res;
+	}
+
+	async findById(id: string): Promise<Recipe> {
+		const recipe = await this.recipeModel.findById(id);
+
+		if (!recipe) {
+			throw new NotFoundException('Receita não encontrada.');
+		}
+
+		return recipe;
+	}
+
+	async updateById(id: string, recipe: Recipe): Promise<Recipe> {
+		return await this.recipeModel.findByIdAndUpdate(id, recipe, {
+			new: true,
+			runValidators: true
+		});
+	}
+
+	async deleteById(id: string): Promise<Recipe> {
+		return await this.recipeModel.findByIdAndDelete(id);
+	}
+}
