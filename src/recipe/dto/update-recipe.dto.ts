5d9e364fcc64a7030f02771ea0aecb49d936641e
Lucas Campolim <lucascampolim@protonmail.com>
2023-11-24 23:50:54 -0300

    feat: organizando validacoes

--- src/recipe/dto/update-recipe.dto.ts
+++ src/recipe/dto/update-recipe.dto.ts
@@ -2,22 +2,22 @@ import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class UpdateRecipeDto {
 	@IsOptional()
-	@IsString({ message: 'O nome está esperando uma string.' })
+	@IsString({ message: 'O campo name está esperando uma string.' })
 	readonly name: string;
 
 	@IsOptional()
-	@IsString({ message: 'Os ingredientes estão esperando uma string.' })
+	@IsString({ message: 'O campo ingredients está esperando uma string.' })
 	readonly ingredients: string;
 
 	@IsOptional()
-	@IsString({ message: 'O método de preparo está esperando uma string.' })
+	@IsString({ message: 'O campo preparationMethod está esperando uma string.' })
 	readonly preparationMethod: string;
 
 	@IsOptional()
-	@IsString({ message: 'A URL da imagem está esperando uma string.' })
+	@IsString({ message: 'O campo imageUrl está esperando uma string.' })
 	readonly imageUrl: string;
 
 	@IsOptional()
-	@IsNumber({}, { message: 'O ID do autor está esperando um number.' })
+	@IsNumber({}, { message: 'O campo idAuthor está esperando um number.' })
 	readonly idAuthor: number;
 }
